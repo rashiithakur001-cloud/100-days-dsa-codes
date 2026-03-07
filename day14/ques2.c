26f212dee0510efa1d5b414bebcc88c9046e15a5
rashiithakur001-cloud <rashiithakur001@gmail.com>
2026-03-07 18:30:24 +0530

    Add files via upload

---
+++ day14/ques2.c
@@ -0,0 +1,31 @@
+/*You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Example 2:
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]*/
+ void rotate(int** matrix, int size, int* colSize) {
+    int dimension = size;
+for (int row = 0; row < dimension; row++) {
+    for (int col = row + 1; col < dimension; col++) {
+ int swapValue = matrix[row][col];
+    matrix[row][col] = matrix[col][row];
+    matrix[col][row] = swapValue;
+}
+    }
+for (int row = 0; row < dimension; row++) {
+    int start = 0;
+    int end = dimension - 1;
+while (start < end) {
+   int swapValue = matrix[row][start];
+   matrix[row][start] = matrix[row][end];
+   matrix[row][end] = swapValue;
+      start++;
+       end--;
+}
+    }
+    return 0;
+}
