c0fc3c6fb390ac2b8acb67873e7c14dad261463e
rashiithakur001-cloud <rashiithakur001@gmail.com>
2026-03-24 21:34:34 +0530

    Add files via upload

---
+++ day48/ques2.c
@@ -0,0 +1,25 @@
+/*Given the root of a binary tree, return the sum of all left leaves.
+A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+Example 2:
+Input: root = [1]
+Output: 0
+*/
+
+int sumOfLeftLeaves(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+    int sum = 0;
+    if (root->left != NULL) {
+        if (root->left->left == NULL && root->left->right == NULL) {
+            sum += root->left->val;
+        } else {
+            sum += sumOfLeftLeaves(root->left);
+        }
+    }
+    sum += sumOfLeftLeaves(root->right);
+    return sum;
+}
