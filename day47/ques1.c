e40be7c2e2c6e1a38242ca2552249f8b6d21c133
rashiithakur001-cloud <rashiithakur001@gmail.com>
2026-03-25 21:26:23 +0530

    Add files via upload

---
+++ day47/ques1.c
@@ -0,0 +1,35 @@
+/*Problem: Height of Binary Tree
+Implement the solution for this problem.
+Input:
+- Input specifications
+Output:
+- Output specifications*/
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+struct Node* createNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    printf("Height of tree: %d\n", height(root));
+    return 0;
+}
