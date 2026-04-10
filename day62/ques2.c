68274e75658aaae218c9c443a2bf6c5a86f0e873
rashiithakur001-cloud <rashiithakur001@gmail.com>
2026-04-10 23:15:59 +0530

    Add files via upload

---
+++ day62/ques2.c
@@ -0,0 +1,61 @@
+/* There are n rooms labeled from 0 to n - 1 and all the rooms are locked except for room 0. Your goal is to visit all the rooms. However, you cannot enter a locked room without having its key.
+
+When you visit a room, you may find a set of distinct keys in it. Each key has a number on it, denoting which room it unlocks, and you can take all of them with you to unlock the other rooms.
+
+Given an array rooms where rooms[i] is the set of keys that you can obtain if you visited room i, return true if you can visit all the rooms, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: rooms = [[1],[2],[3],[]]
+Output: true
+Explanation: 
+We visit room 0 and pick up key 1.
+We then visit room 1 and pick up key 2.
+We then visit room 2 and pick up key 3.
+We then visit room 3.
+Since we were able to visit every room, we return true.
+Example 2:
+
+Input: rooms = [[1,3],[3,0,1],[2],[0]]
+Output: false   */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX 1000
+void dfs(int room, int** rooms, int* roomsColSize, bool visited[]) {
+    visited[room] = true;
+for (int i = 0; i < roomsColSize[room]; i++) {
+        int key = rooms[room][i];
+        if (!visited[key]) {
+            dfs(key, rooms, roomsColSize, visited);
+        }
+    }
+}
+bool canVisitAllRooms(int** rooms, int roomsSize, int* roomsColSize) {
+    bool visited[MAX] = {false};
+    dfs(0, rooms, roomsColSize, visited);
+ // Check if all rooms visited
+ for (int i = 0; i < roomsSize; i++) {
+    if (!visited[i])
+    return false;
+    }
+    return true;
+}
+// Example usage
+int main() {
+    int roomsSize = 4;
+ // Allocate rooms
+    int* rooms[4];
+    int roomsColSize[4] = {1,1,1,0};
+    rooms[0] = (int[]){1};
+    rooms[1] = (int[]){2};
+    rooms[2] = (int[]){3};
+    rooms[3] = (int[]){};
+if (canVisitAllRooms(rooms, roomsSize, roomsColSize))
+    printf("true");
+else
+    printf("false");
+ return 0;
+}
